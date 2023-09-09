bc639762d9f32322f0f25d3269ba0049cb00d7e8
Beren <berenickt99@gmail.com>
2023-09-10 01:50:08 +0900

    초기

---
+++ src/chap_12/clean/Room.java
@@ -0,0 +1,12 @@
+package chap_12.clean;
+
+public class Room {
+
+  public int number = 1;
+
+  public synchronized void clean(String name) {
+    // 직원1 : 3번방 청소 중
+    System.out.println(name + " : " + number + "번방 청소 중");
+    number++;
+  }
+}
