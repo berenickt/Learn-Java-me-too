0001a7aa607110d90b0e691e5e68decba189a42e
Beren <berenickt99@gmail.com>
2023-09-26 00:26:44 +0900

    ADD:_03_Join

--- chap_12/_02_Runnable.java
+++ chap_12/_02_Runnable.java
@@ -9,6 +9,11 @@ import chap_12.clean.CleanRunnable;
 // 2. Runnable 인터페이스 구현
 // -- 클래스 이름 뒷 부분이 extends Thread에서 implements Runnable로 바뀐 것 빼고는 동일
 // -- Runnable 인터페이스는 구현할 메소드가 run() 하나 뿐인 함수형 인터페이스
+//
+// 둘의 차이점은 1번 클래스는 상속을 1개만 받지만,
+// 2번 인터페이스는 여러 번 구현이 가능해서, 다중상속이 가능
+// 정리하면, 구현하는 클래스가 다른 부모 클래스로부터 상속받는다면,
+// Thread 클래스로 상속받는 대신, Runnable 인터페이스로 구현
 public class _02_Runnable {
   public static void main(String[] args) {
     CleanRunnable cleanRunnable = new CleanRunnable();
--- chap_12/_03_Join.java
+++ chap_12/_03_Join.java
@@ -9,6 +9,12 @@ public class _03_Join {
     thread.start();
 
     try {
+      // 📌 thread.join() : 쓰레드가 종료될 떄까지 기다렸다가 끝나면, 다음줄로 넘어감
+      // 멀티 스레드 환경에서 멀티 스레드에 대한 실행 순서를 제어
+      // OS는 멀티 스레드에 대한 실행한 순서를 보장하지 않음
+      //
+      // 만약 thread1.join(); 이라는 코드가 호출됐다면, 여기서의 스레드는 'thread1'이 된다
+      // 즉, 'thread1의 실행이 완료될 때 까지'라는 뜻이
       thread.join(2500); // 2.5 초 대기
     } catch (InterruptedException e) {
       throw new RuntimeException(e);
@@ -23,6 +29,7 @@ public class _03_Join {
       System.out.println("(사장) " + i + "번방 청소 중");
 
       try {
+        // 📌 Thread.sleep() : 지정한 시간동안 프로그램 수행이 잠깐 멈춤
         Thread.sleep(1000);
       } catch (InterruptedException e) {
         throw new RuntimeException(e);
